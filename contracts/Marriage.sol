6be23802c0d82a0f1ae5b2e983f3f0c48049ee27
v.alefman <v.alefman@superdao.co>
2022-12-18 08:30:27 +0200

    first commit

---
+++ contracts/Marriage.sol
@@ -0,0 +1,53 @@
+pragma solidity ^0.8.0;
+
+//import "./interface/ISafe.sol";
+import "./office/Office.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Marriage {
+
+    Office public immutable office;
+    mapping(address=>bool) internal familyAddresses;
+    address [] public familyFunds;
+
+    struct FamilyFund{
+        address fund;
+        ERC20 token;
+    }
+
+    modifier onlyFamilyAddress(){
+        require(familyAddresses[msg.sender], "Only family address");
+        _;
+    }
+
+    constructor(Office _office, address [] memory _familyAddresses) {
+        office = _office;
+        for(uint i; i<_familyAddresses.length; i++){
+            familyAddresses[_familyAddresses[i]]=true;
+        }
+    }
+
+    function deploySafe(address[] memory owners, uint256 threshold)
+    public
+    requirePermission(KERNEL_ADMIN)
+    returns (address)
+    {
+        bytes memory emptyBytes;
+        bytes memory initial = abi.encodeWithSelector(
+            ISafe.setup.selector,
+            owners,
+            threshold,
+            address(0),
+            emptyBytes,
+            office.safeFallbackHandler(),
+            address(0),
+            0,
+            payable(address(0))
+        );
+        ISafe safe = office.safeFactory().createProxy(address(office.safeSingleton()), initial);
+        familyFunds.push(address(safe));
+        emit SafeDeployed(address(safe));
+
+        return address(safe);
+    }
+}
---
+++ contracts/office/Office.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.8.0;
+
+import "../interface/ISafe.sol";
+
+contract Office {
+    ISafe public immutable safeSingleton;
+    ISafeFactory public immutable safeFactory;
+    address public immutable safeFallbackHandler;
+
+    constructor(
+        ISafe safeSingleton_,
+        ISafeFactory safeFactory_,
+        address safeFallbackHandler_
+    ) {
+        safeSingleton = safeSingleton_;
+        safeFactory = safeFactory_;
+        safeFallbackHandler = safeFallbackHandler_;
+    }
+}
